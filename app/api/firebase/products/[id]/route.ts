8ec00c7dbfa5c67e225808d727fcad690965946b
Swathi M <swathi.m1@leadsquared.com>
2025-03-25 21:58:19 +0530

    fixing build

--- app/api/firebase/products/[id]/route.ts
+++ app/api/firebase/products/[id]/route.ts
@@ -1,15 +1,20 @@
 import { getCache, setCache } from "@/lib/cache";
 import { db } from "@/lib/firebase"; // Adjust based on your Firebase setup
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const CACHE_EXPIRY = 3600 * 1000;
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const CACHE_PATH = `cache/products/${params.id}`;
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export async function GET(request: NextRequest, props: Props) {
+  const id = (await props.params)?.id;
+
+  const CACHE_PATH = `cache/products/${id}`;
 
   try {
     const cachedProduct = await getCache(CACHE_PATH, CACHE_EXPIRY);
@@ -21,12 +26,12 @@ export async function GET(
 
     const productQuery = query(
       collection(db, "Products"),
-      where("id", "==", params.id)
+      where("id", "==", id)
     );
 
     const productDoc = (await getDocs(productQuery))?.docs?.[0];
     if (productDoc.exists()) {
-      const productData = { id: params.id, ...productDoc.data() };
+      const productData = { id: id, ...productDoc.data() };
       await setCache(CACHE_PATH, productData);
       return NextResponse.json(productData);
     }
